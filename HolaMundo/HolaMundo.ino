0e7990b58bf4bf7ba4552e615db4cecf0bde72ba
RobertoCarlos12 <robert_102105@hotmail.com>
2014-10-19 13:13:01 -0500

    no message

--- HolaMundo/HolaMundo.ino
+++ HolaMundo/HolaMundo.ino
@@ -1,12 +1,13 @@
+int led=13;
 void setup() {
   // put your setup code here, to run once:
-pinMode(13,OUTPUT);
+pinMode(led,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(13,HIGH);
+digitalWrite(led,HIGH);
 delay(500);
-digitalWrite(13,LOW);
+digitalWrite(led,LOW);
 delay(500);
 }
